7bbcbb963225591aaa36ba990cadb95470b39b3d
Balagonikavya <142068067+Balagonikavya@users.noreply.github.com>
2024-03-09 14:13:28 +0530

    V.0

---
+++ LoginPage.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import YourComponent from './YourComponent'
+import './styles.css'
+
+export default function LoginPage() {
+  return (
+    <div>
+      <YourComponent/>
+    </div>
+  )
+}
